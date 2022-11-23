3ddeaba07d0b82f473cb6b55b00695c252899422
dev-freya <freya.colle@gmail.com>
2022-11-23 09:59:42 +0800

    Two Sum: hashmap js

---
+++ two-sum.js
@@ -0,0 +1,22 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order. 
+*/
+
+var twoSum = function(nums, target) {
+    // Initialize new Object
+    const hashMap = {};
+    for (let i=0; i < nums.length; i++) {
+        let num = nums[i];
+        let diff = target - num;
+        // if diff in hashMap, return it
+        if (hashMap[diff] !== undefined) {
+            return [hashMap[diff], i];
+        } else {
+            hashMap[num] = i;
+        }
+    }
+}
